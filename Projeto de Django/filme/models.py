095241c0080c19d3520c2d8722c524fea37db198
Vincy de Tarsis <vincydetarsis@gmail.com>
2023-07-10 06:24:48 -0300

    Filme
    Foi criado o primeiro filme

--- Projeto de Django/filme/models.py
+++ Projeto de Django/filme/models.py
@@ -1,9 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-
+LISTA_CATEGORIA = (
+    ("ANALISES", "Análises"),
+    ("PROGRAMACAO", "Programação"),
+    ("APRESENTACAO", "Apresentação"),
+    ("OUTROS", "Outros"),
+)
 #Criar o filme
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='thumb_filmes')
+    descricao = models.TextField(max_length=1000)
+    categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIA)
+    visualizacoes = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.titulo
 #Criar os episodios
 
 #Criar usuarios
