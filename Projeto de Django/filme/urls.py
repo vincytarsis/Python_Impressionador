c745a798548811e22dba8e78fe3913dec3f3dd53
Vincy de Tarsis <vincydetarsis@gmail.com>
2023-07-19 06:23:24 -0300

    homefilems

--- Projeto de Django/filme/urls.py
+++ Projeto de Django/filme/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path, include
-from .views import homepage
+from .views import homepage, homefilmes
 
 urlpatterns = [
     path('', homepage),
+
+    path('filmes/', homefilmes),
 ]
